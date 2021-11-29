d403cbc5f5b89196f5073f1f6294cb087efe81db
Yanying Wang <yanyingwang1@gmail.com>
2021-11-29 15:25:16 +0800

    css modifying code

--- content_script.js
+++ content_script.js
@@ -4,12 +4,16 @@ console.log(jQuery().jquery);
 
 /////// actions /////////
 handler = function main() {
-  debugger;
+  $(".tocset").css("position", "fixed");
+  $(".tocset").css("overflow-y", "scroll");
+  $(".tocset").css("height", "100vh");
+  $(".tocset").css("margin-top", "7rem");
+  $(".tocset").css("padding-top", "1rem");
+  $(".navsettop").css("position", "fixed");
+  $(".navsettop").css("background", "#a7b0be");
+  $(".navsettop").css("padding-bottom", "1rem");
 }
 
 // $( handler );
 $(document).ready(function() { $( handler ); });
 
-
-// $(.tocset).style.append("position: fixed; overflow-y: scroll; height: 100vh;")
-// $(.navsettop).style.append("position: fixed;")

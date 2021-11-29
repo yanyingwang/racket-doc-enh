b771c9e5a9970099b3eeb927bc06fa22d12f6aed
Yanying Wang <yanyingwang1@gmail.com>
2021-11-29 15:58:31 +0800

    fix css values

--- content_script.js
+++ content_script.js
@@ -4,14 +4,16 @@ console.log(jQuery().jquery);
 
 /////// actions /////////
 handler = function main() {
-  $(".tocset").css("position", "fixed");
-  $(".tocset").css("overflow-y", "scroll");
-  $(".tocset").css("height", "100vh");
-  $(".tocset").css("margin-top", "7rem");
-  $(".tocset").css("padding-top", "1rem");
   $(".navsettop").css("position", "fixed");
   $(".navsettop").css("background", "#a7b0be");
-  $(".navsettop").css("padding-bottom", "1rem");
+  $(".navsettop").css("padding-bottom", "0.5rem");
+
+  $(".tocset").css("position", "fixed");
+  $(".tocset").css("overflow-y", "scroll");
+  $(".tocset").css("height", "87vh");
+  $(".tocset").css("margin-top", "6.5rem");
+  $(".tocset").css("padding-top", "0.5rem");
+  $(".tocset").css("padding-bottom", "0.5rem");
 }
 
 // $( handler );
